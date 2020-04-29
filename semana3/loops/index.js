f326d8071db1805c99e3766a53e48a9a9d32c82b
Diogo Gaspar <ghostlabz@gmail.com>
2020-04-29 20:39:32 -0300

    Completo, faltando 2 desafios

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -6,16 +6,17 @@ O que o código abaixo está fazendo? Qual o resultado impresso no console?
 
 // Soma todos os números de 1 até 14.
 
-/* let sum = 0;
+let sum = 0;
 for (let i = 0; i < 15; i++) {
   sum += i;
 }
 console.log(sum);
- */
+
+/* EXERCÍCIO 2 */
 
 /* Leia o código abaixo: */
 
-/* const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30];
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30];
 const novaLista = [];
 const numero = 4;
 for (const item of lista) {
@@ -23,7 +24,7 @@ for (const item of lista) {
     novaLista.push(item);
   }
 }
-console.log(novaLista); */
+console.log(novaLista);
 
 /* 
 a. O que o comando `.push` faz?
@@ -43,23 +44,95 @@ Multiplos de 4: [12]
 /* EXERCÍCIO 3  */
 
 //Este array será usado para exemplificar as respostas de todos os itens
+
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 
 //a. Escreva um programa que devolva o maior e o menor números contidos no array original
-//let num = 0;
-let menor = 0;
+
+let menor = array[0];
 let maior = array[0];
 
 for (let i = 0; i < array.length; i++) {
-  if (array[i] < array[menor]) {
+  if (array[i] > maior) {
+    maior = array[i];
+  }
+  if (array[i] < menor) {
     menor = array[i];
-
-    console.log(menor);
   }
 }
+console.log(`O maior número é ${maior}, é o menor é ${menor}`);
 
 //b. Escreva um programa que devolva um novo array contendo todos os valores do array original divididos por 10.
 
+let divisao = [];
+
+for (let i = 0; i < array.length; i++) {
+  divisao[i] = array[i] / 10;
+}
+
+console.log(divisao);
+
 //c. Escreva um programa que devolva um novo array contendo, somente, os números pares do array original.
 
+let pares = [];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    pares[i] = array[i];
+  }
+}
+console.log(pares);
+
 //d. Escreva um programa que gere um novo array contendo strings, da seguinte forma: "O elemento do índex i é: numero"
+
+let str = [];
+let i = 0;
+while (i < array.length) {
+  let str = `O elemento do index ${i} e ${array[i]} `;
+  console.log(str);
+
+  i++;
+}
+
+/* Desafio 1 
+
+Resposta:
+
+0
+00
+000
+0000
+00000
+ */
+
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "));
+let quantidadeAtual = 0;
+while (quantidadeAtual < quantidadeTotal) {
+  let linha = "";
+  for (let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++) {
+    linha += "0";
+  }
+  console.log(linha);
+  quantidadeAtual++;
+}
+
+//Desafio 2
+
+let num1 = prompt("Diga um número: ");
+
+let num2 = prompt("Vamos Jogar!: ");
+
+if (num1 > num2) {
+  console.log(
+    `O número chutado foi: ${num2}
+    Errou, é maior!`
+  );
+}
+if (num1 < num2) {
+  console.log(
+    `O  número chutado foi: ${num2}
+      Errou, é menor`
+  );
+} else {
+  console.log(`ACERTOU!!!`);
+}
