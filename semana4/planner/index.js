2b3e167922482fa12d4126f91dbfc06e2fcc82ac
Diogo Gaspar <ghostlabz@gmail.com>
2020-05-05 17:00:10 -0300

    To do

---
+++ semana4/planner/index.js
@@ -0,0 +1,40 @@
+function dailyTask() {
+  let task = document.querySelector("input").value;
+  let text = document.createTextNode(task);
+  let newTask = document.createElement("li");
+  newTask.appendChild(text);
+  //document.querySelector("#seg").appendChild(newTask);
+
+  let weekDay = document.querySelector("select").value;
+
+  switch (weekDay) {
+    case "1":
+      document.querySelector("#dom").appendChild(newTask);
+      break;
+
+    case "2":
+      document.querySelector("#seg").appendChild(newTask);
+      break;
+
+    case "3":
+      document.querySelector("#ter").appendChild(newTask);
+      break;
+
+    case "4":
+      document.querySelector("#qua").appendChild(newTask);
+      break;
+
+    case "5":
+      document.querySelector("#qui").appendChild(newTask);
+      break;
+
+    case "6":
+      document.querySelector("#sex").appendChild(newTask);
+      break;
+
+    case "7":
+      document.querySelector("#sab").appendChild(newTask);
+      break;
+  }
+  document.querySelector("input").value = "";
+}
