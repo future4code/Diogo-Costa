9e62796baa3abbd4c5e6ef6effab686844055d15
Diogo Gaspar <ghostlabz@gmail.com>
2020-05-06 16:52:41 -0300

    Faltaram essas partes

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,23 +1,15 @@
-const dados = {
+function getData() {
+  event.preventDefault();
+  const titulo = document.getElementById("title").value;
+  const autor = document.querySelector("#author").value;
+  const postagem = document.querySelector("#post").value;
+
+  console.log();
+}
+
+/* const dados = {
   titulo: "",
   autor: "",
   postagem: "",
 };
-
-function getTitle(event) {
-  //event.preventDefault();
-  const titulo = document.querySelector("#title").value;
-  const autor = document.querySelector("#author").value;
-  const postagem = document.querySelector("#post").value;
-
-  const dadosCompletos = {
-    //...dados,
-    titulo: titulo,
-    autor: autor,
-    postagem: postagem,
-  };
-
-  console.log(dadosCompletos);
-}
-
-//getTitle();
+ */
