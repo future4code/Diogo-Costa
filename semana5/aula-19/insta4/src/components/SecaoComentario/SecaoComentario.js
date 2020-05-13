9c80be3e45670154366f1a2ecff58d46dde4f25d
Diogo Gaspar <ghostlabz@gmail.com>
2020-05-13 16:49:32 -0300

    Instabenu

---
+++ semana5/aula-19/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./SecaoComentario.css";
+
+export class SecaoComentario extends Component {
+  state = {
+    comentario: "",
+  };
+
+  onChangeComentario = (event) => {
+    const comentarioFeito = event.target.value;
+    this.setState({ comentario: comentarioFeito });
+    console.log(this.state.comentario);
+  };
+
+  /* Não consegui fazer implimir só uma linha no console.
+  
+  imprimeNoConsole = (event) => {
+    const comentarioFeito = event.target.value;
+    console.log(comentarioFeito);
+  }; */
+
+  render() {
+    return (
+      <div className={"comment-container"}>
+        <input
+          className={"input-comentario"}
+          placeholder={"Comentário"}
+          value={this.setState.comentario}
+          onChange={this.onChangeComentario}
+          //onClick={this.imprimeNoConsole}
+        />
+        <button onClick={this.props.aoEnviar}>Enviar</button>
+      </div>
+    );
+  }
+}
