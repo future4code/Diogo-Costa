112a7197e158fe93b1c4f1cb193dc12a7e9eb673
Diogo Gaspar <ghostlabz@gmail.com>
2020-06-08 10:26:29 -0300

    os arquivos da build estão no stage

---
+++ semana7/aula-quinta-api/build/precache-manifest.477bcf6f21925b3669b71bbff893d819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff4f164b2eaf6e9a77e453729f05cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9e403f0c01f11ff3633",
+    "url": "/static/css/main.6e5a4e93.chunk.css"
+  },
+  {
+    "revision": "44a07eae4db9f16992db",
+    "url": "/static/js/2.ba0e4699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba0e4699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e403f0c01f11ff3633",
+    "url": "/static/js/main.90645199.chunk.js"
+  },
+  {
+    "revision": "f531a4ce7aab717bd04a",
+    "url": "/static/js/runtime-main.c10befe7.js"
+  }
+]);
---
+++ semana7/labefy/build/precache-manifest.378d7af9b288f95de48062d3290b74cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219566db4c279bc5d34129018f8dc879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d13219d3baf62774f8",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "1684cd6f1b07fe2cfa64",
+    "url": "/static/js/2.73014634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73014634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d13219d3baf62774f8",
+    "url": "/static/js/main.fa11a500.chunk.js"
+  },
+  {
+    "revision": "eb3c23879d0d5180d9e1",
+    "url": "/static/js/runtime-main.d526d959.js"
+  }
+]);
