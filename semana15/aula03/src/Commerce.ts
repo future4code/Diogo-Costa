e56fa5904c6e322665e10e753a6f3ac5276d1097
Diogo Gaspar <ghostlabz@gmail.com>
2020-08-05 16:13:51 -0300

    Semana15-aula03

---
+++ semana15/aula03/src/Commerce.ts
@@ -0,0 +1,10 @@
+export class Commerce extends Place {
+	constructor(
+		protected floorsQuantity: number,
+		// Refere-se à quantidade de andares do lugar
+
+		cep: string
+	) {
+		super(cep);
+	}
+}
